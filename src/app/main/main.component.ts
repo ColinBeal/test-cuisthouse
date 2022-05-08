cd1436518408e6a89169b2d8cad2ba3600b3e4a8
Colin Beal <colinbealwork@gmail.com>
2022-05-08 17:52:30 +0200

    [APP] Add all the content missing in main

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import  { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
@@ -7,4 +9,27 @@ import { Component } from '@angular/core';
 })
 
 export class MainComponent {
+  public cards: Recipe[] = [
+    {
+      _id: 1,
+      label: 'Tarte aux pommes et frangipane',
+      category: 'Dessert',
+      url: 'url(https://raw.githubusercontent.com/ColinBeal/test-cuisthouse/master/src/assets/img/tarte.jpg)',
+      difficultyRating: 2
+    },
+    {
+      _id: 2,
+      label: 'Tarte aux pommes et frangipane',
+      category: 'Dessert',
+      url: 'url(https://raw.githubusercontent.com/ColinBeal/test-cuisthouse/master/src/assets/img/tarte.jpg)',
+      difficultyRating: 1
+    },
+    {
+      _id: 3,
+      label: 'Tarte aux pommes et frangipane',
+      category: 'Dessert',
+      url: 'url(https://raw.githubusercontent.com/ColinBeal/test-cuisthouse/master/src/assets/img/tarte.jpg)',
+      difficultyRating: 4
+    },
+  ]
 }
