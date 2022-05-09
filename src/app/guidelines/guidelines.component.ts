7106920f38532aaa20ec94c764615244bfb48627
Colin Beal <colinbealwork@gmail.com>
2022-05-09 22:25:00 +0200

    [Guidelines] Add all components to the guidelines

--- src/app/guidelines/guidelines.component.ts
+++ src/app/guidelines/guidelines.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-guidelines',
@@ -7,4 +8,48 @@ import { Component } from '@angular/core';
 })
 
 export class GuidelinesComponent {
+  public cards: Recipe[] = [
+    {
+      _id: 1,
+      label: 'Apple frangipane tart',
+      category: 'Desert',
+      url: 'url(https://raw.githubusercontent.com/ColinBeal/test-cuisthouse/master/src/assets/img/tarte.jpg)',
+      difficultyRating: 2
+    },
+    {
+      _id: 2,
+      label: 'Apple frangipane tart',
+      category: 'Dessert',
+      url: 'url(https://raw.githubusercontent.com/ColinBeal/test-cuisthouse/master/src/assets/img/tarte.jpg)',
+      difficultyRating: 1
+    },
+    {
+      _id: 3,
+      label: 'Apple frangipane tart',
+      category: 'Dessert',
+      url: 'url(https://raw.githubusercontent.com/ColinBeal/test-cuisthouse/master/src/assets/img/tarte.jpg)',
+      difficultyRating: 4
+    },
+    {
+      _id: 1,
+      label: 'Apple frangipane tart',
+      category: 'Desert',
+      url: 'url(https://raw.githubusercontent.com/ColinBeal/test-cuisthouse/master/src/assets/img/tarte.jpg)',
+      difficultyRating: 2
+    },
+    {
+      _id: 2,
+      label: 'Apple frangipane tart',
+      category: 'Dessert',
+      url: 'url(https://raw.githubusercontent.com/ColinBeal/test-cuisthouse/master/src/assets/img/tarte.jpg)',
+      difficultyRating: 1
+    },
+    {
+      _id: 3,
+      label: 'Apple frangipane tart',
+      category: 'Dessert',
+      url: 'url(https://raw.githubusercontent.com/ColinBeal/test-cuisthouse/master/src/assets/img/tarte.jpg)',
+      difficultyRating: 4
+    },
+  ]
 }
